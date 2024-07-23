9a438a27e004e70bcd207b013d62cb120ecee0cc
sakamoto7777 <ssakamoto7657@gmail.com>
2024-07-23 22:40:37 +0900

    mdn変更

---
+++ dom_00/dom.js
@@ -0,0 +1,13 @@
+// head要素を取得し、中身を出力する
+console.log(document.head);
+// body要素を取得し、中身を出力する
+console.log(document.body);
+
+// windowオブジェクトの中身を出力する
+console.log(window);
+
+ // HTML要素をidで取得し、中身を出力する
+ console.log(document.getElementById('second-list'));
+
+ // HTML要素をclassで取得し、中身を出力する
+ console.log(document.getElementsByClassName('heading'));
